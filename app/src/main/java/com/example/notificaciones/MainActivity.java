1d9886d1448c1c373e3f1b008025bd65bff7e6ff
JCLancho <jclancho0@gmail.com>
2019-11-04 12:20:19 +0100

    ejercicio notificaciones terminado

--- app/src/main/java/com/example/notificaciones/MainActivity.java
+++ app/src/main/java/com/example/notificaciones/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.notificaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView lblAcertadasValor, lblPrimerDigito, lblSegundoDigito,
             lblSignoOperacion, inputSolucion;
 
-    private int resultadoCorrecto, resultadoUsuario;
+    private Button btnComprobar;
+
+    private int resultadoCorrecto;
+
+    private NotificationManager notificationManager;
+    static final String CANAL_ID ="mi_canal";
+    static final int NOTIFICACION_ID =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +35,28 @@ public class MainActivity extends AppCompatActivity {
         lblSegundoDigito = findViewById(R.id.lblSegundoDigito);
         lblSignoOperacion = findViewById(R.id.lblSignoOperacion);
         inputSolucion = findViewById(R.id.inputSolucion);
-        generarOperacion(this.findViewById(R.id.Vista));
+        btnComprobar = findViewById(R.id.btnComprobar);
+        generarOperacion();
     }
 
-    public void generarOperacion(View v){
+    public void generarOperacion(){
 
         int signo = (int) (Math.random()*4);
-        String[] signos = {"+","-","*","/"};
+        String[] signos = {"+", "-", "*", "/"};
         lblSignoOperacion.setText(signos[signo]);
 
         int num1 = (int) (Math.random()*100);
-        int num2 = (int) (Math.random()*100);
+        int num2 = (int) ((Math.random()*99)+1);
+
+
 
         if(signo == 3){
+            Log.i("que hago", "estoy dividiendo");
             while(num1 % num2 != 0){
                 num1 = (int) (Math.random()*100);
-                num2 = (int) (Math.random()*100);
+                num2 = (int) ((Math.random()*99)+1);
             }
         }
-
 
 
         lblPrimerDigito.setText(num1+"");
@@ -54,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             case "-":
                 resultadoCorrecto = num1 - num2;
                 break;
-            case "x":
+            case "*":
                 resultadoCorrecto = num1 * num2;
                 break;
             case "/":
@@ -68,9 +83,46 @@ public class MainActivity extends AppCompatActivity {
 
     public void comprobar(View v){
 
-        if(resultadoCorrecto == resultadoUsuario){
-            lblAcertadasValor.setText((Integer.parseInt(lblAcertadasValor.getText().toString())+1)+"");
+        if(!inputSolucion.getText().toString().equals("")) {
+            int resultadoUsuario = Integer.parseInt(inputSolucion.getText().toString());
+            if (resultadoCorrecto == resultadoUsuario) {
+                lblAcertadasValor.setText((Integer.parseInt(lblAcertadasValor.getText().toString()) + 1) + "");
+            }
+            if (lblAcertadasValor.getText().toString().equals("10")) {
+                notificar();
+                btnComprobar.setEnabled(false);
+                inputSolucion.setEnabled(false);
+            } else {
+                inputSolucion.setText("");
+                generarOperacion();
+            }
+        }else{
+            Toast toast = Toast.makeText(getApplicationContext(), "Introduce algun resultado", Toast.LENGTH_SHORT);
+            toast.show();
         }
+    }
+
+    private void notificar(){
+
+        //Creamos notificacion
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        //Creamos el canal. SOLO puede hacerse en dispositivos con ver.8 o más.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CANAL_ID,
+                    "Mis notificaciones",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setDescription("Descripción del canal");
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificacion = new NotificationCompat.Builder(MainActivity.this,CANAL_ID)
+                .setSmallIcon(R.drawable.icono)
+                .setContentTitle("Enhorabuena!!")
+                .setContentText("Has acertado 10 operaciones!!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        notificationManager.notify(NOTIFICACION_ID, notificacion.build());
 
     }
 
